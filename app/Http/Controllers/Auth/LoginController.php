f7b3c7cc5d424dd79a36889ce38f6c401eedb238
Admin <nidhish@gmail.com>
2017-08-26 09:52:54 +0530

    Initialize

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * To authenticate a user for stateless request
+     *
+     * @return bool
+     */
+    public function verifyApiUser(Request $request)
+    {
+        if ( Auth::attempt( [
+            'email' => $request->get('email'),
+            'password' => $request->get('password')
+        ] ) ) {
+            return json_encode(['status' => true]);
+        } else {
+            return json_encode(['status' => false]);
+        }
+    }
+}
